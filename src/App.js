0c34ffa374b8a51396bb678c8ab7beb4b83c60d5
Paul Savage <savagep88@gmail.com>
2020-11-23 13:32:52 -0800

    navbar first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand } from 'reactstrap'; 
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="warning">
+        <div className="container">
+          <NavbarBrand href="/">Jupes</NavbarBrand>  
+        </div>  
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
